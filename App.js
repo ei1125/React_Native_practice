4ff7a52abed45e23c57c23662f9b402f0f0e01b4
ei1125 <i.dont.like.monday1125@gmail.com>
2020-05-17 12:56:29 +0900

    4-22レイアウトの区画整理

--- App.js
+++ App.js
@@ -1,26 +1,40 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  itemContainer: {
+    height: 100,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    flexDirection: 'row'
+  },
+  leftContainer: {
+    backgroundColor: 'red',
+    width: 100
+  },
+  rightContainer: {
+    backgroundColor: 'blue',
+    flex: 1
+  }
+});
+
 export default function App() {
-    return (
-        <View style={styles.container}>
-            <View
-                style={{
-                    height: 100,
-                    width: '100%',
-                    borderColor: 'gray',
-                    borderWidth: 1,
-                }}
-            ></View>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <View style={styles.itemContainer}>
+        <View style={styles.leftContainer} />
+        <View style={styles.rightContainer} />
+      </View>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+
+

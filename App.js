0812f22bc7680132d845e59e6c2ff4012fd4976c
ei1125 <i.dont.like.monday1125@gmail.com>
2020-05-20 00:20:48 +0900

    6-40 Axiosを使ってAPIを実行する
    axiosのインストール
    imageUrlのエラー対策

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native';
 import ListItem from './components/ListItem';
 import dummyArticles from './dummies/articles';
 import Constants from 'expo-constants';
+import axios from 'axios';
+
+const URL = `http://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.manifest.extra.newsApiKey}`
 
 const styles = StyleSheet.create({
   container: {
@@ -37,13 +40,17 @@ const styles = StyleSheet.create({
 export default function App() {
   const [articles, setArticles] = useState([]);
   useEffect(() => {
-    alert(Constants.manifest.extra.newsApiKey)
-    const timer = setTimeout(() => {
-      setArticles(dummyArticles);
-    }, 2000);
-    return () => clearTimeout(timer);
+    fetchArticles();
   }, []);
-
+  
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      setArticles(response.data.articles)
+    } catch (error) {
+      console.error(error);
+    }
+  }
   return (
     <SafeAreaView style={styles.container}>
       <FlatList

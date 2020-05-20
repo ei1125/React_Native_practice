07b88b0063c5981e49592f10470a6aecfe541d8a
ei1125 <i.dont.like.monday1125@gmail.com>
2020-05-20 22:32:35 +0900

    7-47 StackNavigatorの利用

--- components/ListItem.js
+++ components/ListItem.js
@@ -2,12 +2,6 @@ import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-    backgroundColor: '#fff',
-  },
   itemContainer: {
     height: 100,
     width: '100%',

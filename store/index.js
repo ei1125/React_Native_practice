43a4c5de03c06ab2b298922d30bd8ebed210ac61
ei1125 <i.dont.like.monday1125@gmail.com>
2020-05-22 00:53:37 +0900

    8-58 Storeの実装

---
+++ store/index.js
@@ -0,0 +1,11 @@
+//ツリー構造をつくる
+import {createStore, combineReducers} from  'redux';
+import userReducer from './reducers/user';
+
+const rootReducer = combineReducers ({
+  user: userReducer
+})
+
+const store = createStore(rootReducer);
+
+export default store;

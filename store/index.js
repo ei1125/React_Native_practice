7acd249af5cf5d805dd05949cd7939d7f450f6fe
ei1125 <i.dont.like.monday1125@gmail.com>
2020-05-22 23:49:49 +0900

    8-59 デバッガーのインストール

--- store/index.js
+++ store/index.js
@@ -1,11 +1,12 @@
 //ツリー構造をつくる
 import {createStore, combineReducers} from  'redux';
 import userReducer from './reducers/user';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const rootReducer = combineReducers ({
-  user: userReducer
+  user: userReducer,
 })
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
